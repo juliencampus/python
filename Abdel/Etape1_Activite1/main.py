ca92bf8ebf40348861451fea5c2714d6e5eac95f
AbdelkaderA <abdelkader.atamaa@le-campus-numerique.fr>
2020-10-13 14:17:01 +0200

    1er enoncer

--- Abdel/Etape1_Activite1/main.py
+++ Abdel/Etape1_Activite1/main.py
@@ -1,13 +1,16 @@
+import math
+
+
+
 def prix_produit(prixht, taxe):
-
-    prixTTC = prixht + ((prixht * taxe)/100)
+    taxeArrondiSup = math.ceil(taxe)
+    prixTTC = prixht + ((prixht * taxeArrondiSup) / 100)
     return prixTTC
-
 
 def main():
     """Le programme principal."""
 
-    #nom_produit = input("Nom du produit : ")  # une chaîne de caractères
+    # nom_produit = input("Nom du produit : ")  # une chaîne de caractères
 
     nom_produit = input("Nom du produit : ")
     prixht = input("Prix HT : ")
@@ -19,9 +22,7 @@ def main():
     # calculer le périmètre
     prixTTC = prix_produit(prixht, taxe)
 
-
-    print("Le prix TTC du produit", nom_produit, "est de", prixTTC, "€ (taxes de", taxe, "%)")
-
+    print("Le prix TTC du produit", nom_produit, "est de", prixTTC, "€ (taxes de", taxeArrondiSup, "%)")
 
 
 if __name__ == "__main__":
