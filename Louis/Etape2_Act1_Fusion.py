3fbeb57e637fc1c869407cef096b2bad0ac45d01
Ducournl <louis.ducournau@le-campus-numerique.fr>
2020-10-14 14:14:23 +0200

    Etape 2 - Activité 1

---
+++ Louis/Etape2_Act1_Fusion.py
@@ -0,0 +1,18 @@
+
+import itertools as it
+
+def main():
+
+
+    list_int = list(range(10))
+    list_str = ["Jason", "Marty", "Kevin", "Barbara", "Gouniafé"]
+
+    print(list_int)
+
+
+    print([x for x in it.chain.from_iterable(it.zip_longest(list_int, list_str)) if x])
+
+
+
+if __name__ == "__main__":
+    main()
