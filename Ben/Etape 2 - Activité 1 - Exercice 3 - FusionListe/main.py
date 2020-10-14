89ae1f8dac30d16a589b2343a54673927d78d5a2
FerrandezBenjamin <ferrandez.benjamin@le-campus-numerique.fr>
2020-10-14 13:26:26 +0200

    Rangement Fichier

---
+++ Ben/Etape 2 - Activité 1 - Exercice 3 - FusionListe/main.py
@@ -0,0 +1,11 @@
+def main():
+    liste_entier = [1, 2 ,3 ,4 ,5]
+    liste_character = ["Ulys", "Abdel", "Mlo", "Clem", "desolepourlesautrejailaflemmedemettretlm"]
+    liste_entier.extend( liste_character)
+    print(liste_entier)
+
+
+
+
+
+main()
--- Ben/Exo5/main.py
+++
@@ -1,6 +0,0 @@
-def jesuislindex():
-    L = [15, 17, 18, 24, 51, 14, 96]
-    A = 51
-    print(L.index(A))
-
-jesuislindex()
