51cf06b8b1d7af91fe242f850f733b4724f1df19
FerrandezBenjamin <ferrandez.benjamin@le-campus-numerique.fr>
2020-10-14 13:43:17 +0200

    FusionList finit

--- Ben/Etape 2 - Activité 1 - Exercice 3 - FusionListe/main.py
+++ Ben/Etape 2 - Activité 1 - Exercice 3 - FusionListe/main.py
@@ -1,11 +1,9 @@
+import itertools as it
+
 def main():
     liste_entier = [1, 2 ,3 ,4 ,5]
     liste_character = ["Ulys", "Abdel", "Mlo", "Clem", "desolepourlesautrejailaflemmedemettretlm"]
-    liste_entier.extend( liste_character)
-    print(liste_entier)
-
-
-
-
+    ma_liste = [ x for x in it.chain.from_iterable(it.zip_longest(liste_entier, liste_character)) if x]
+    print(ma_liste)
 
 main()
