b26f2f4653f7c421dec4e4074daed487b8ad5ec1
FerrandezBenjamin <ferrandez.benjamin@le-campus-numerique.fr>
2020-10-14 15:09:26 +0200

    Best Vendeur finit

--- Ben/Etape 2 - Activité 2 - Exercice 1 - Ventes/main.py
+++ Ben/Etape 2 - Activité 2 - Exercice 1 - Ventes/main.py
@@ -1,3 +1,6 @@
+from collections import Counter
+import operator
+
 dico = {}
 dico ["Theo"] = 15
 dico ["Clement"] = 35
@@ -5,9 +8,18 @@ dico ["Ulys"] = 42
 dico ["Mlo"] = 16
 dico ["Abdel"] = 72
 
+    # Afficher les valeurs des clef : dictionnaire.values()
+    # Afficher la clef : dictionnaire.keys()
+    # Afficher OrdreCroissant : Counter(Dictionnaire)
 
 def totalvente():
     compter = sum(dico.values())
     print(compter)
 
-totalvente()
+def meilleurvendeur():
+    pseudo = dico.keys()
+    bestvendeur = max(*sorted(enumerate(pseudo), key=operator.itemgetter(1)))
+    print(f"Le meilleur vendeur est : {bestvendeur}")
+
+
+meilleurvendeur()
