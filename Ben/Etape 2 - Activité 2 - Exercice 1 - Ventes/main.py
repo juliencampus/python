1d71a320068fb52329f75a4614c2b7afdc9b90de
FerrandezBenjamin <ferrandez.benjamin@le-campus-numerique.fr>
2020-10-14 16:22:31 +0200

    Chaine etudiant finit

--- Ben/Etape 2 - Activité 2 - Exercice 1 - Ventes/main.py
+++ Ben/Etape 2 - Activité 2 - Exercice 1 - Ventes/main.py
@@ -12,14 +12,29 @@ dico ["Abdel"] = 72
     # Afficher la clef : dictionnaire.keys()
     # Afficher OrdreCroissant : Counter(Dictionnaire)
 
+
+# Fonction calculer le total de vente :
+
 def totalvente():
     compter = sum(dico.values())
     print(compter)
+
+# Fonction qu'affiche le meilleur vendeur parmis un dicionnaire :
 
 def meilleurvendeur():
     pseudo = dico.keys()
     bestvendeur = max(*sorted(enumerate(pseudo), key=operator.itemgetter(1)))
     print(f"Le meilleur vendeur est : {bestvendeur}")
 
+# Fonction qui permet de transformer une chaine de caractère en dictionnaire (en gros) :
 
-meilleurvendeur()
+def numeroetudiant():
+    chaine_etudiants = """213615200;BESNIER;JEAN
+    213565488;DUPOND;MARC
+    214665555;DURAND;JULIE"""
+    print({str.split(";")[0]: str.split(";")[1] + str.split(";")[2] for str in chaine_etudiants.split("\n")})
+
+
+
+
+numeroetudiant()
