9c02d94d6645f840ec1b9fe94f7a4d5f4d281330
VBLab <vbourgeon@hotmail.fr>
2020-10-16 17:02:36 +0200

    jusqu'à étape 3bis

---
+++ Virginie/etape3_act1.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+
+
+print("Veuillez entrer le côté a : ")
+a = float(input())
+print("Veuillez entrer le côté b : ")
+b = float(input())
+print("Veuillez entrer le côté c : ")
+c = float(input())
+d = (a + b + c) / 2  # demi-périmètre
+s = sqrt(d * (d - a) * (d - b) * (d - c))  # aire (suivant formule)
+
+print("Longueur des côtés =", a, b, c)
+print("Périmètre =", d * 2, "Aire =", s)
+
+if __name__ == "__main__":
+        main()
