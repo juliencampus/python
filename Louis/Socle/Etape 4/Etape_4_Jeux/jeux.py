6e39c2a52cb761c65fab3664ecb893f0b3b35dea
Ducournl <louis.ducournau@le-campus-numerique.fr>
2020-10-19 15:55:26 +0200

    Démarrage Django

---
+++ Louis/Socle/Etape 4/Etape_4_Jeux/jeux.py
@@ -0,0 +1,8 @@
+import joueur
+
+class Jeux(object):
+    def __init__(self):
+        pass
+    # setters
+
+
---
+++ Louis/Socle/Etape 4/Etape_4_Jeux/main.py
@@ -0,0 +1,11 @@
+import joueur
+
+
+def main():
+    a = joueur.Wizard()
+    print(round(a.armMg))
+
+
+
+if __name__ == "__main__":
+        main()
