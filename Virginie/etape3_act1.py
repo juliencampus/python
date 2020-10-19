c3f9664764ed2466130e7f22a323868d6ff3aa3b
VBLab <vbourgeon@hotmail.fr>
2020-10-19 11:29:04 +0200

    lundi 19/10

--- Virginie/etape3_act1.py
+++ Virginie/etape3_act1.py
@@ -1,18 +1,20 @@
 from math import sqrt
 
 
+def aire_peri_rect():
 
-print("Veuillez entrer le côté a : ")
-a = float(input())
-print("Veuillez entrer le côté b : ")
-b = float(input())
-print("Veuillez entrer le côté c : ")
-c = float(input())
-d = (a + b + c) / 2  # demi-périmètre
-s = sqrt(d * (d - a) * (d - b) * (d - c))  # aire (suivant formule)
 
-print("Longueur des côtés =", a, b, c)
-print("Périmètre =", d * 2, "Aire =", s)
+    print("Veuillez entrer le côté a : ")
+    a = float(input())
+    print("Veuillez entrer le côté b : ")
+    b = float(input())
+    print("Veuillez entrer le côté c : ")
+    c = float(input())
+    d = (a + b + c) / 2  # demi-périmètre
+    s = sqrt(d * (d - a) * (d - b) * (d - c))  # aire (suivant formule)
+
+    print("Longueur des côtés =", a, b, c)
+    print("Périmètre =", d * 2, "Aire =", s)
 
 if __name__ == "__main__":
-        main()
+    aire_peri_rect()
