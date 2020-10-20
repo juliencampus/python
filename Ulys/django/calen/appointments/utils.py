ad9d92279b9e1fecad1e4088bf03bd10d52c1955
Virginie <ulys.chambard@gmail.com>
2020-10-20 16:22:12 +0200

    stash

--- Ulys/django/calen/appointments/utils.py
+++ Ulys/django/calen/appointments/utils.py
@@ -4,9 +4,10 @@ from .models import Appointment
 
 
 class Calendar(HTMLCalendar):
-    def __init__(self, year=None, month=None):
+    def __init__(self, year=None, month=None, week=None):
         self.year = year
         self.month = month
+        self.week = week
         super(Calendar, self).__init__()
 
     def formatday(self, day, appointments):
@@ -18,17 +19,23 @@ class Calendar(HTMLCalendar):
             return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
         return '<td></td>'
 
-    def formatweek(self, theweek, appointments):
-        week = ''
-        for d, weekday in theweek:
-            week += self.formatday(d, appointments)
-        return f'<tr> {week} </tr>'
-
-    def formatmonth(self, withyear=True):
-        appointments = Appointment.objects.filter(starts_at__year=self.year, starts_at__month=self.month)
+    def formatweek(self, withyear=True):
+        appointments = Appointment.objects.filter(starts_at__year=self.year, starts_at__month=self.month, starts_at__week=self.week)
         cal = f'<table border="0" cellpadding="0" cellspacing="0"     class="calendar">\n'
         cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
         cal += f'{self.formatweekheader()}\n'
-        for week in self.monthdays2calendar(self.year, self.month):
-            cal += f'{self.formatweek(week, appointments)}\n'
+        theweek = [datetime.today() - timedelta(day=x) for x in range(0, 3)] + [datetime.today() + timedelta(day=x) for x in range(0,3)]
+        week = ''
+        for d, weekday in theweek:
+            week += self.formatday(d, appointments)
+        cal += f'<tr> {week} </tr>'
         return cal
+
+    # def formatmonth(self, withyear=True):
+    #     appointments = Appointment.objects.filter(starts_at__year=self.year, starts_at__month=self.month)
+    #     cal = f'<table border="0" cellpadding="0" cellspacing="0"     class="calendar">\n'
+    #     cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
+    #     cal += f'{self.formatweekheader()}\n'
+    #     for week in self.monthdays2calendar(self.year, self.month):
+    #         cal += f'{self.formatweek(week, appointments)}\n'
+    #     return cal
