d57aa676e9269e19955c7300ee4c3ddc95b15edf
Virginie <ulys.chambard@gmail.com>
2020-10-22 10:59:29 +0200

    vue display + get List endpoint

--- Ulys/django/apnter/apnter_front/src/store/index.js
+++ Ulys/django/apnter/apnter_front/src/store/index.js
@@ -16,10 +16,13 @@ export default new Vuex.Store({
   actions: {
     getAppointments({ commit }){
       var config = {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
-        }
+        // headers: {
+          "Access-Control-Allow-Origin": "*",
+        //   "Access-Control-Allow-Credentials": "true",
+        //   "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+        //   "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,access-control-allow-origin",
+        //   // 'Content-Type': 'application/json',
+        // }
       };
       axios
         .get('http://127.0.0.1:8000/appointments', config)
