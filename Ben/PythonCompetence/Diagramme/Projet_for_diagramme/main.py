6c8f05710c4ed618c3a60dc9729956c125596b16
FerrandezBenjamin <ferrandez.benjamin@le-campus-numerique.fr>
2020-10-22 16:32:53 +0200

    Compétence UML + le programme

--- Ben/PythonCompetence/Diagramme/Projet_for_diagramme/main.py
+++ Ben/PythonCompetence/Diagramme/Projet_for_diagramme/main.py
@@ -1,55 +1,61 @@
 
-class Animal:
-    def __init__(self, nom):
+class Animal():
+    nom =" "
+    nombrePattes = 0
+    vitesseMax = 0
+    def __init__(self, nom, nombrePattes, vitesseMax):
         self.nom = nom
-        self.age
 
     def __str__(self):
-        return "{0} {1} ".format(self.nom, self.age)
+        return "{0} {1}".format(self.nom, self.nombrePattes)
 
 
 class Chien(Animal):
+    vitesseMax = 70
+    nombrePattes = 4
 
-    def __init__(self, nom, vitessemax):
-        Animal.__init__(self, nom)
-        self.vitessemax = vitessemax
-
-    def __str__(self):
-        return "{0} {1} ".format(self.nom, self.vitessemax)
-
-
-    def makeChien():
-        nom = input(f'Le nom de ton chien : ')
-        age = input(f"Entre l'age de ton chien :")
-        print(f"Ton chien hérite de Animal, son nom est", nom, 'et a :', age, 'ans.')
-        return nom, age
-
-
-
-class Chat(Animal):
-
-    def __init__(self, nom, vitessemax):
-        Animal.__init__(self, nom)
-        self.vitessemax = vitessemax
+    def __init__(self, nom, nombrePattes, vitesseMax):
+        Animal.__init__(self, nom, nombrePattes, vitesseMax)
 
     def __str__(self):
-        return "{0} {1} ".format(self.nom, self.vitessemax)
+        return "{0} {1} {2} ".format(self.nom, self.nombrePattes, self.vitesseMax)
 
 
-    def makeChat():
-        nom = input(f'Le nom de ton chat : ')
-        age = input(f"Entre l'age de ton chat :")
-        print(f"Ton chat hérite de Animal, son nom est :", {nom}, 'et a :', {age}, 'ans.')
-        return nom, age
+    def makeChien(self):
+        self.nom = input(f'Le nom de ton chien : ')
+        print(f"Ton chien hérite d'Animal, son nom est :", self.nom, "\n Il possède une pointe de vitesse de : ", self.vitesseMax, "\n Il possède", self.nombrePattes, "pattes")
+
+
+
+class Poule(Animal):
+    vitesseMax = 15
+    nombrePattes = 2
+
+    def __init__(self, nom, nombrePattes, vitesseMax):
+        Animal.__init__(self, nom, nombrePattes, vitesseMax)
+
+    def __str__(self):
+        return "{0} {1} {2} ".format(self.nom, self.nombrePattes, self.vitesseMax)
+
+
+    def makePoule(self):
+        self.nom = input(f'Le nom de ta poule : ')
+        print(f"Ta poule hérite d'Animal, son nom est :", self.nom, "\n Elle possède une pointe de vitesse de: ", self.vitesseMax, "\n Elle possède", self.nombrePattes, "pattes")
 
 
 def makeAnimal():
-    choix = int(input(f'Tape 1 pour créer un chien : \nTape 2 pour créer un chat : '))
-    if choix == 1:
-        Chien.makeChien()
-    elif choix == 2:
-        Chat.makeChat()
+    noms = ["", "chien", "poule"]
+    choixAnimal = int(input(f'Tape 1 pour créer un chien : \nTape 2 pour créer une poule : '))
+    chien = Chien(noms[choixAnimal], 0, 0)
+    poule = Poule(noms[choixAnimal], 0, 0)
+    try:
+        if choixAnimal == 1:
+            Chien.makeChien(chien)
+        elif choixAnimal == 2:
+            Poule.makePoule(poule)
+    except ValueError:
+        print('Entre nombre stp frr')
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     makeAnimal()
