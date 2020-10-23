08a128010392418ee3e724d4d23f0c62cb82f107
Clement-Chalencon <clement.chalencon@le-campus-numerique.fr>
2020-10-23 16:08:41 +0200

    super

--- Clément/testFull/cal/forms.py
+++ Clément/testFull/cal/forms.py
@@ -5,7 +5,7 @@ from .models import Event
 class EventForm(ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'description', 'start', 'end', 'allday']
+        fields = ['title', 'description', 'start', 'end']
 
     def __init__(self, *args, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
@@ -13,4 +13,20 @@ class EventForm(ModelForm):
         self.fields['description'].widget.attrs.update({'class': 'form-control'})
         self.fields['start'].widget.attrs.update({'class': 'form-control'})
         self.fields['end'].widget.attrs.update({'class': 'form-control'})
-        self.fields['allday'].widget.attrs.update({'class': 'form-check-input', 'type': 'checkbox'})
+        # self.fields['allday'].widget.attrs.update({'class': 'form-check-input', 'type': 'checkbox', 'style': 'margin-left:10px'})
+
+        error_messages = {
+            'application': {
+                'required': ("Application field is required"),
+            },
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start")
+        end_date = cleaned_data.get("end")
+
+        if start_date and end_date and end_date < start_date:
+            raise forms.ValidationError(
+                    "end_date should be greater than start_date."
+                )
--- Clément/testFull/cal/views.py
+++ Clément/testFull/cal/views.py
@@ -20,41 +20,24 @@ def index(request):
     return render(request, 'cal/index.html', context)
 
 
-def detail(request, event_id):
-    event = Event.objects.get(pk=event_id)
-    e = {'id': event.id, 'title': event.title, 'description': event.description,
-         'start_day': event.start.strftime("%d / %m / %Y"),
-         'start_time': event.start.strftime("%H : %M"),
-         'end_day': event.end.strftime("%d / %m / %Y"),
-         'end_time': event.end.strftime("%H : %M"),
-         'allday': event.allday,
-         }
-    context = {'event_detail': e}
-    return render(request, 'cal/detail.html', context)
-
-
-def delete(request, event_id):
-    Event.objects.filter(pk=event_id).delete()
-    return index(request)
-
 
 def event_form(request, event_id=False):
     if not event_id:
         if request.method == 'GET':
             form = EventForm()
             return render(request, 'cal/event_form.html', {'form': form, 'creation': True})
-        else :
+        else:
             f = EventForm(request.POST)
             f.save()
             return index(request)
     else:
         event = Event.objects.get(pk=event_id)
         if request.method == 'GET':
-            form = EventForm(initial={'title': event.title, 'description': event.description, 'start': event.start, 'end': event.end, 'allday': event.allday})
+            form = EventForm(initial={'title': event.title, 'description': event.description, 'start': event.start, 'end': event.end}).is_valid()
             return render(request, 'cal/event_form.html', {'form': form, 'creation': False})
         else:
             instance = get_object_or_404(Event, id=event_id)
-            f = EventForm(request.POST, instance=instance)
+            f = EventForm(request.POST, instance=instance).is_valid()
             if request.POST.get("update"):
                 f.save()
             elif request.POST.get("delete"):
