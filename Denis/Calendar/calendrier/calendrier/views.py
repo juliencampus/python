eb15e58b608a4c241ec453dc2d32baeaab775713
DenisBeliant <denis.beliant@le-campus-numerique.fr>
2020-10-23 15:50:42 +0200

    Beta version

--- Denis/Calendar/calendrier/calendrier/views.py
+++ Denis/Calendar/calendrier/calendrier/views.py
@@ -22,7 +22,10 @@ def changeCreneau(rdv, jour):
     for el in rdv:
         totalTemps = round(el.types / 15)
         for i in range(0, totalTemps):
-            creneaux[findIndex(el.heure) + i] = (datetime.datetime(hour=el.heure.hour, day=1, minute=0, year=2020, month=4) + datetime.timedelta(minutes= i * 15), el.patient, 'bg-secondary')
+            try:
+                creneaux[findIndex(el.heure) + i] = (datetime.datetime(hour=el.heure.hour, day=1, minute=0, year=2020, month=4) + datetime.timedelta(minutes= i * 15), el.patient, 'bg-secondary')
+            except:
+                break
 
     return creneaux
 
@@ -65,6 +68,26 @@ def takerdv(request, idHeure, date):
 def saveRdv(request):
     f = RdvForm(request.POST)
     date = request.POST.get('date')
-    f.save()
+    heure = '2020-10-20 '+request.POST.get('heure')
+    heureTotal = datetime.datetime.strptime(heure, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(minutes=int(request.POST.get('types')))
+
+    heuresRdv = Rdv.objects.filter(date__day = datetime.datetime.strptime(date, '%Y-%m-%d').day)
+
+    isEmpty = True
+
+    type(heure)
+    type(date)
+
+    for el in heuresRdv:
+        heureRdv = datetime.datetime.strptime(date, '%Y-%m-%d') + datetime.timedelta(hours=el.heure.hour, minutes=el.heure.minute)
+        if heureTotal > heureRdv:
+            isEmpty = False
+            break
+
+
+    if isEmpty:
+        f.save()
+    else:
+        return HttpResponse("C'est déja prit !")
 
     return redirect('../'+date)
