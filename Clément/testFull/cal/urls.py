288fcc413331ee4c7fea37abd5c11d4dd10bbe97
Clement-Chalencon <clement.chalencon@le-campus-numerique.fr>
2020-10-23 09:14:21 +0200

    forms

--- Clément/testFull/cal/urls.py
+++ Clément/testFull/cal/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('calendar/', views.index, name='index'),
     path(f'calendar/detail/<int:event_id>/', views.detail, name='detail'),
+    path(f'calendar/detail/<int:event_id>/delete', views.delete, name='delete'),
+    path(f'calendar/new', views.create, name='create'),
 ]
--- Clément/testFull/cal/views.py
+++ Clément/testFull/cal/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from cal.models import Event
+from cal.forms import EventForm
 import json
 
 
@@ -10,15 +11,38 @@ def index(request):
     events_list = []
     for event in events:
         e = {'id': event.id, 'title': event.title, 'description': event.description,
-             'start': event.start.strftime("%Y-%m-%dT%H:%M:%S"),
-             'end': event.end.strftime("%Y-%m-%dT%H:%M:%S"), 'allday': event.allday, 'url': f'http://127.0.0.1:8000/calendar/detail/{event.id}/'}
+             'start': event.start.strftime("%Y-%m-%dT%H:%M"),
+             'end': event.end.strftime("%Y-%m-%dT%H:%M"),
+             'allday': event.allday,
+             'url': f'http://127.0.0.1:8000/calendar/detail/{event.id}/'}
         events_list.append(e)
+        print(json.dumps(events_list))
     context = {'format_events': json.dumps(events_list)}
     return render(request, 'cal/index.html', context)
 
 
 def detail(request, event_id):
     event = Event.objects.get(pk=event_id)
-    print(event.title)
-    context = {'event_detail': event}
+    e = {'id': event.id, 'title': event.title, 'description': event.description,
+         'start_day': event.start.strftime("%d / %m / %Y"),
+         'start_time': event.start.strftime("%H : %M"),
+         'end_day': event.end.strftime("%d / %m / %Y"),
+         'end_time': event.end.strftime("%H : %M"),
+         'allday': event.allday,
+         }
+    context = {'event_detail': e}
     return render(request, 'cal/detail.html', context)
+
+
+def delete(request, event_id):
+    Event.objects.filter(pk=event_id).delete()
+    return index(request)
+
+
+def create(request):
+    form = EventForm()
+    return render(request, 'cal/event_form.html', {'form': form})
+    # print(request.POST)
+    # string.join(request.PUT['start_time'])
+    # print(string)
+
